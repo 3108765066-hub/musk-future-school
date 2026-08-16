2c825cbe93a4356c2efe6dd2090f3308989b2496
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-16 14:21:57 +0800

    Add real cat interaction sounds

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786860404|6439312';
+const CACHE_VERSION = '1786861267|3006803';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
