82201b610ce3e6f751d7c6370d52f41e703df9a8
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-17 14:19:15 +0800

    Add directional pet sprites and prevent character sticking

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786945689|5761010';
+const CACHE_VERSION = '1786947421|9507321';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
