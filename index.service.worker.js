b08c5d25a5ee6ee15b5f2202c70a443a07879607
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-18 03:29:24 +0800

    Use grounded football walking animations

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786993940|1921762';
+const CACHE_VERSION = '1786994889|1875589';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
