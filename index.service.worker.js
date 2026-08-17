66be3bbaee0f126683875405055ea6d7c1b90f20
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-18 03:04:34 +0800

    Rename default cat and raise pet page

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786991458|1849200';
+const CACHE_VERSION = '1786993357|1981111';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
