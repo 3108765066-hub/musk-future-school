7ea548ba82702834e295f2786d90f937b9d873ec
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-17 15:31:58 +0800

    Show lesson reward before retake

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786948993|7361966';
+const CACHE_VERSION = '1786951855|3919778';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
