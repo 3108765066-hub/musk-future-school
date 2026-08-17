bf8c7fea7e0640668280794d723a5ccbc011660c
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-18 00:25:14 +0800

    Allow conversations to close without reopening

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786982884|2002416';
+const CACHE_VERSION = '1786983790|1838691';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
