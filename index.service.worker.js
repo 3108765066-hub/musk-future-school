5ae44bf6b624e71912a6642e2e136aba1c61fe57
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-18 03:13:39 +0800

    Reduce cat size across growth stages

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786993357|1981111';
+const CACHE_VERSION = '1786993940|1921762';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
