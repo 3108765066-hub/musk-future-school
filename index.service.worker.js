c3b76d4277954fb8d1a7cc610b8b51e224ed37b2
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-17 21:56:26 +0800

    Fix engineer lab route

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786960536|2095154';
+const CACHE_VERSION = '1786974913|6269785';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
