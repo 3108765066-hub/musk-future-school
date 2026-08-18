d545037360fd90566c092327e4099724433ddbbd
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-18 22:53:27 +0800

    Match dialogue portrait to player appearance

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787063032|5292172';
+const CACHE_VERSION = '1787064721|3379217';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
