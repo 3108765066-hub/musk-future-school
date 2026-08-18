0d2418bf4f0a25fdf3f62bcdc07cf00b7b446d5d
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-18 09:32:50 +0800

    Add side portrait teacher dialogue

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1786996027|2086507';
+const CACHE_VERSION = '1787016742|3089796';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
