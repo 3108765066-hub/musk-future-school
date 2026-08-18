7d21baae3499e2e9d0b1290818ac55866772ba0f
阮经天 <ruanjingtian@Deep-Learning.local>
2026-08-18 22:24:42 +0800

    Fix lab NPC animation and online vision flow

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787045850|3783996';
+const CACHE_VERSION = '1787063032|5292172';
 /** @type {string} */
 const CACHE_PREFIX = '马斯克的未来学校-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
